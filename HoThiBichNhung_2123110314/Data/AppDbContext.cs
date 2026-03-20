960fd61f74f2afc414de1551306f337ff253e80b
BichNhung263 <bichnhung263@gmail.com>
2026-03-20 16:07:08 -0300

    Add project files.

---
+++ HoThiBichNhung_2123110314/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HoThiBichNhung_2123110314.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HoThiBichNhung_2123110314.Data
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
